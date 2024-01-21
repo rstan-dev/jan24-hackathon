2e1bcd13a5debc7a676c955bb5d290de6cf2ca6e
Richard Marles <marleswebinfo@gmail.com>
2024-01-21 14:55:26 +0100

    pulling from jackob

--- home/views.py
+++ home/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 def index(request):
     """ A view to return the index page """
 
-    return render(request, 'home/index.html')
+    return render(request, 'allauth/account/login.html')
