727399f53a9e9800faefa4944cdbd1bca732d52d
bek <bekrye@gmail.com>
2024-01-21 11:05:15 +0100

    Added Savings page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import account, expenses
+from .views import account, expenses, savings
 
 urlpatterns = [
 
     path('', account, name='profile'),
     path('expense_summary/', expenses, name='expense'),
+    path('saving_summary/', savings, name='saving'),
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -34,6 +34,23 @@ def expenses(request):
 
     return render(request, 'accounts/expense.html', context)
 
+def savings(request):
+    # Assume you have logic to calculate total_expenses for each category
+    # and pass it to the template context
+    categories = Category.objects.all()
+    if request.method == 'POST':
+        category_name = request.POST.get('category_name')
+        total_saving = calculate_total_saving(request.POST, category_name)
+    else:
+        total_saving = None
+
+    context = {
+        'categories': categories,
+        'total_saving': total_saving,
+    }
+
+    return render(request, 'accounts/saving.html', context)
+
 def calculate_total_expense(post_data, category_name):
     total_expense = 0
 
@@ -46,4 +63,16 @@ def calculate_total_expense(post_data, category_name):
 
     return total_expense
 
+def calculate_total_saving(post_data, category_name):
+    total_saving = 0
+
+    for key, value in post_data.items():
+        if key.startswith(f'savingAmount_{category_name}_'):
+            try:
+                total_expenses = calculate_total_expense(category_name)
+                total_saving = total_expenses - budget
+            except ValueError:
+                pass  # Ignore invalid values
+
+    return total_saving
     
