fc8febc67d9f579158ed51c674743ea56fa38230
JakBul <bjakub68@gmail.com>
2024-01-21 13:14:45 +0000

    Update forms.py

--- home/views.py
+++ home/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-
+from . import templates
 # Create your views here.
 
 def index(request):
     """ A view to return the index page """
 
-    return render(request, 'home/index.html')
+    return render(request, 'account/login.html')
