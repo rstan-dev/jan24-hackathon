188b30872b489a025fbaa4548927bd6ba97642f3
Richard Marles <marleswebinfo@gmail.com>
2024-01-21 18:15:59 +0100

    Style background image of the page styling the menu

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from . import templates
+# from . import templates
 # Create your views here.
+
 
 def index(request):
     """ A view to return the index page """
