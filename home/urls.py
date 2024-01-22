700dcc3310be4d8b74798957db4b73fb91c073c6
SamOBrienOlinger <samtobo@yahoo.co.uk>
2024-01-22 11:11:49 +0000

    Fix about-us.html link in footer.

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from .views import about_us
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('about-us.html/', about_us, name='about_us'),
 ]
--- home/views.py
+++ home/views.py
@@ -7,3 +7,8 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'account/login.html')
+
+def about_us(request):
+    """ A view to return the about page """
+    
+    return render(request, 'home/about-us.html') 
